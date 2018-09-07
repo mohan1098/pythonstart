f127f0b8dea602f3793a96c7facdcb95cc9e2fd3
Madan Mohan <mohan1098@gmail.com>
2018-09-07 15:57:40 +0530

    Adding Escape uses

---
+++ escape.py
@@ -0,0 +1,9 @@
+print("My name is 'madan' Mohan Pandey") #We can not use " in " but we can use single quote
+print('My name is "madan" Mohan Pandey') #We can not use "single quote" in single quote but we can use double quote
+print("My name is \"Madan\" pandey") #Using back slash for escape sqquense
+print("My name is \'Madan pandey") #Using back slash for escape sqquense alway use \' or \" when needed
+print("My name is madan\nand i am learner") #Using \n for new line
+print("Using\tfor tab it will give the space between using and for") #Using \t for tab
+print("Using\bfor tab it will give the backspace between using and for") #Using \b for tab
+print("Using for tab\\") #in case we need backslash in last
+print("Hello \\n madan")  #If we need \ as normal text then we need to add two \\
